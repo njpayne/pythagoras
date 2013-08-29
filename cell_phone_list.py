7aa27d7aaec1e6790bf673158efbbeeb1b1e3688
Nathaniel Payne <njpayne@sfu.ca>
2013-08-28 21:42:06 -0700

    Storing objects in a list

---
+++ cell_phone_list.py
@@ -0,0 +1,7 @@
+# It is common practice to make all of a class's data attributes private and to provide methods for accessing and changing those attributes
+# A method that returns a value from a class's attribute but does not change it is known as an accessor method
+# Note that get_manufact, get_model, and get_retail_price are alll accessor methods
+# On the other hand, a method that stores a value in a data attribute or changes the value of a data attribute
+# in some other way is known as a mutator method. These methods control the way that a class's data attributes are modified.
+# set_manufac, set_model, and set_retail are mutator methods
+# Note that mutator methods are sometimes called "setters" and accessor methods are sometimes called "getters"
